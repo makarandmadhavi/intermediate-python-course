b3c1599f0b12a5ec63ea9ad65f213fc899d9a134
Makarand <30976763+makarandmadhavi@users.noreply.github.com>
2020-09-04 12:07:48 +0530

    add 2 player name and player score

--- dice_roller.py
+++ dice_roller.py
@@ -1,17 +1,26 @@
 import random
 def main():
+  dice_sum = 0
+  pname = [0,0]
+  pscore = [0,0]
+  pname[0] = input('Player 1 what is your name? ')
+  pname[1] = input('Player 2 what should we call you? ')
   dicerolls = int(input('How many dice would you like to roll? '))
   dice_size = int(input('How many sides are the dice? '))
-  dice_sum = 0
-  for i in range(0,dicerolls):
-    roll= random.randint(1,dice_size)
-    dice_sum += roll
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
-    elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
-    else:
-      print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+  for j in range(0,2):
+    for i in range(0,dicerolls):
+      roll= random.randint(1,dice_size)
+      pscore[j] += roll
+      if roll == 1:
+        print(f'{pname[j]} rolled a {roll}! Critical Fail')
+      elif roll == dice_size:
+        print(f'{pname[j]} rolled a {roll}! Critical Success!')
+      else:
+        print(f'{pname[j]} rolled a {roll}')
+    print(f'{pname[j]} have rolled a total of {dice_sum}')
+  if(pscore[0]>pscore[1]):
+    print(f'{pname[0]} Wins!')
+  else:
+    print(f'{pname[1]} Wins!')
 if __name__== "__main__":
   main()
